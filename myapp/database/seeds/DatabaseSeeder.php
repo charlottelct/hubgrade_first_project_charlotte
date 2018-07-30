31f72bb8f6048d136d01b579f6ac4c9ba332dabb
charlottelct <41893837+charlottelct@users.noreply.github.com>
2018-07-30 16:51:44 +0200

    [Laravel] First database test : databse seeding

---
+++ myapp/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        class DatabaseSeeder extends Seeder
+        {
+            public function run()
+            {
+                $this->call(ArticlesTableSeeder::class);
+                $this->call(UsersTableSeeder::class);
+            }
+        }
+    }
+}
