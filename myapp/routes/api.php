a481345b185344764bc5418ceebcfe7da0543dd3
charlottelct <41893837+charlottelct@users.noreply.github.com>
2018-07-31 13:41:26 +0200

    [Laravel] : . Create a welcome page as well as a /new page

--- myapp/routes/api.php
+++ myapp/routes/api.php
@@ -16,13 +16,13 @@ Use App\Article;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
- 
+
 Route::get('articles', function() {
     // If the Content-Type and Accept headers are set to 'application/json', 
     // this will return a JSON structure. This will be cleaned up later.
     return Article::all();
 });
- 
+
 Route::get('articles/{id}', function($id) {
     return Article::find($id);
 });
